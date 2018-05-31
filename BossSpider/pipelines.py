ada68fd8b1e00e71f4adc106d170764bed0c1e38
wahlmzr <wahlmzr@163.com>
2018-05-31 00:57:27 -0400

    start linux

---
+++ BossSpider/pipelines.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+'''
+import sqlite3
+
+class BossspiderPipeline(object):
+    #打开爬虫时，连接数据库connect('database_name')
+    def open_spider(self,spider):
+        self.con = sqlite3.connect("zhipin.sqlite")
+        self.cu = self.con.cursor()
+
+
+    def process_item(self, item, spider):
+        #sql插入语句，将爬取的数据插入到数据库
+        insert_sql = "insert into zhipin (post,salary,company,area,exp,edu,industry) VALUES ('{}','{}','{}','{}','{}','{}','{}')"\
+            .format(item['post'],item['salary'],item['company'],item['area'],item['exp'],item['edu'],item['industry'])
+        #执行插入语句
+        self.cu.execute(insert_sql)
+        #数据提交（数据插入或更新）需要commit
+        self.con.commit()
+        return item
+
+    #爬虫结束时，数据库关闭
+    def spider_close(self,spider):
+
+        self.con.close()
+
+'''
+
+import pymysql
+
+class BossspiderPipeline(object):
+    #打开爬虫时，连接数据库connect('database_name')
+    def open_spider(self,spider):
+        self.con = pymysql.connect('192.168.20.107','root','1987','boss', charset='utf8')
+        self.cu = self.con.cursor()
+
+
+    def process_item(self, item, spider):
+        #sql插入语句，将爬取的数据插入到数据库
+        insert_sql = "insert into zhipin (post,salary,company,area,exp,edu,industry,detail ) VALUES ('{}','{}','{}','{}','{}','{}','{}')"\
+            .format(item['post'],item['salary'],item['company'],item['area'],item['exp'],item['edu'],item['industry'] , item['detail_page'])
+        #执行插入语句
+        self.cu.execute(insert_sql)
+        #数据提交（数据插入或更新）需要commit
+        self.con.commit()
+        return item
+
+    #爬虫结束时，数据库关闭
+    def spider_close(self,spider):
+
+        self.con.close()
---
+++ BossSpider/spiders/zhipin.py
@@ -0,0 +1,46 @@
+# encoding=utf-8
+
+import time
+import scrapy
+#引入容器来保存所爬去的信息
+from ..items import BossspiderItem
+
+class ZhiPinSpider(scrapy.Spider):
+    #设置唯一标示name
+    name = 'BossSpider'
+    #设置爬取的地址
+    start_urls = ['https://www.zhipin.com/c101020100/h_100010000/?query=数据分析&page={cnt}&ka=page-next'.format(cnt=cnt)
+                  for cnt in range(1,16)]
+
+    def parse(self, response):
+        '''
+        爬取方法
+        '''
+        #容器保存爬取的信息
+        zp = BossspiderItem()
+        #爬取部分：使用xpath获取每个职位的li
+        for posts in response.xpath(".//*[@id='main']/div/div[2]/ul/li/div"):
+            #
+            #获取职位
+            zp['post'] = posts.xpath(".//div[1]/h3/a/div[1]/text()").extract()[0]
+            #获取工资
+            zp['salary'] = posts.xpath(".//div[1]/h3/a/span/text()").extract()[0]
+            #获取公司
+            zp['company'] = posts.xpath(".//div[1]/h3/a/text()").extract()[-1]
+            #获取地区
+            
+            zp['area'] = posts.xpath(".//div[1]/p/text()").extract()[0]
+            #获取需求经验
+            zp['exp'] = posts.xpath(".//div[1]/p/text()").extract()[1]
+            #获取所需学历
+            zp['edu'] = posts.xpath(".//div[1]/p/text()").extract()[2]
+            #获取行业
+            zp['industry'] = posts.xpath(".//div[1]/p/text()").extract()[3]
+            
+            zp['detail_page'] = posts.xpath('//*[@id="main"]/div/div[2]/ul/li[1]/div/div[1]/h3/a/text').extract()[0]
+            
+            #返回信息
+            yield zp
+            time.sleep(5)
+            
+
